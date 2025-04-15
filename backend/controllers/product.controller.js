dc3296be227e02e1412ce20a00724af4f40a0d8e
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2025-04-15 13:11:12 +0000

    first testing in postman working

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -2,63 +2,64 @@ const Product = require('../models/product.model');
 
 // Obtener todos los productos
 exports.getAllProducts = async (req, res) => {
-    try {
-        const products = await Product.find();
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({ message: 'Erros al obtener los productos', error: error.message });
-    }
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener los productos', error: error.message });
+  }
 };
 
 // Obtener un producto específico por ID
 exports.getProductById = async (req, res) => {
-    try {
-        const product = await Product.findById(req.params.id);
-        if (!product) {
-            return res.status(404).json({ message: 'Product no encontrado' });
-        }
-    } catch (error) {
-        res.status(500).json({ message: 'Error al obtener el producto', error: error.message });
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
     }
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: 'Error al obtener el producto', error: error.message });
+  }
 };
 
 // Crear un nuevo producto
 exports.createProduct = async (req, res) => {
-    try {
-        const newProduct = new Product(req.body);
-        const savedProduct = await newProduct.save();
-        res.status(201).json(savedProduct);
-    } catch (error) {
-        res.status(400).json({ message: 'Error al crear el producto', error: error.message })
-    }
+  try {
+    const newProduct = new Product(req.body);
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct); // 201 Created
+  } catch (error) {
+    res.status(400).json({ message: 'Error al crear el producto', error: error.message }); // 400 Bad Request
+  }
 };
 
 // Actualizar un producto existente
 exports.updateProduct = async (req, res) => {
-    try {
-        const updatedProduct = await Product.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            { new: true, runValidators: true } 
-        );
-        if (!updatedProduct) {
-            return res.status(404).json({ message: 'Producto no encontrado' });
-        }
-        res.status(200).json(updatedProduct);
-    } catch (error) {
-        res.status(400).json({ message: 'Error al actualizar el producto', error: error.message });
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true } // new: true para devolver el documento actualizado
+    );
+    if (!updatedProduct) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
     }
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(400).json({ message: 'Error al actualizar el producto', error: error.message });
+  }
 };
 
 // Eliminar un producto
 exports.deleteProduct = async (req, res) => {
-    try {
-        const deletedProduct = await Product.findByIdAndDelete(req.params.id);
-        if (!deletedProduct) {
-            return res.status(404).json({ message: 'Producto no encontrado' });
-        }
-        res.status(204).send();
-    } catch (error) {
-        res.status(500).json({ message: 'Error al eliminar el producto', error: error.message });
+  try {
+    const deletedProduct = await Product.findByIdAndDelete(req.params.id);
+    if (!deletedProduct) {
+      return res.status(404).json({ message: 'Producto no encontrado' });
     }
+    res.status(204).send(); // 204 No Content (la operación fue exitosa pero no hay contenido para devolver)
+  } catch (error) {
+    res.status(500).json({ message: 'Error al eliminar el producto', error: error.message });
+  }
 };
--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
-    name: { type: String, require: true },
-    description: {type: String, required: true },
-    price: {type: Number, required: true },
-    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
-    image: {type: String},
-    stock: {type: Number, required: true, default: 0 },
-    createAt: {type: Date, default: Date.now },
-    updateAt: {type: Date, default: Date.now }
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  category: { type: String, required: true },
+  image: { type: String },
+  stock: { type: Number, required: true, default: 0 },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
 
 const Product = mongoose.model('Product', productSchema);
 
-module.export = Product;
+module.exports = Product;
